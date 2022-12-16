5d907c62a4f6c2e419a6737edf015904341c7bb6
Bianeck <thiagobianeck@gmail.com>
2022-12-16 20:48:39 -0300

    Initial commit

---
+++ src/main/java/br/com/bieniek/miniauthorizer/MiniAuthorizerApplication.java
@@ -0,0 +1,13 @@
+package br.com.bieniek.miniauthorizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniAuthorizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniAuthorizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bieniek/miniauthorizer/MiniAuthorizerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bieniek.miniauthorizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAuthorizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
